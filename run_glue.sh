c63e869b2a747b314768ddee5811af3b72f4f89b
yue.li.12 <ardellelee@outlook.com>
2019-08-26 16:19:11 +0800

    update script

--- run_glue.sh
+++ run_glue.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -eux
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=3
 export GLUE_DIR=dataset/glue
 export TASK_NAME=MRPC
 
@@ -14,8 +14,8 @@ python ./examples/run_glue.py \
     --do_lower_case \
     --data_dir $GLUE_DIR/$TASK_NAME \
     --max_seq_length 128 \
-    --per_gpu_eval_batch_size=8   \
-    --per_gpu_train_batch_size=8   \
+    --per_gpu_eval_batch_size 16   \
+    --per_gpu_train_batch_size 16   \
     --learning_rate 2e-5 \
     --num_train_epochs 3.0 \
     --output_dir output/$TASK_NAME/
